0234742f310892f93f1c9e3f3bf77549cd2b67b3
Syifa Fauzia Tahir <95572816+Syifatahir@users.noreply.github.com>
2025-08-12 16:26:11 +0700

    Add files via upload

---
+++ javascript-sql/Jobsheet/latihan1-js-dasar/tipe-data.js
@@ -0,0 +1,15 @@
+let nama = "Daisy";
+let umur = 25;
+let lulus = true;
+let nilai = null;
+let alamat;
+let hobi = ["Baca", "Nyanyi"];
+let siswa = { nama: "Syifa", kelas: "Web"};
+
+console.log("Nama:", nama);
+console.log("umur:", umur);
+console.log("lulus:", lulus);
+console.log("nilai:", nilai);
+console.log("alamat:", alamat);
+console.log("hobi:", hobi);
+console.log("siswa:", siswa);
